5820a616e79466d12165f3c3e41df08a70b63177
Phoenix <theemaraldblock@gmail.com>
2019-05-05 15:50:10 +0100

    Update Dogebot.js

--- Dogebot.js
+++ Dogebot.js
@@ -1,4 +1,26 @@
 const Discord = require('discord.js')
 const doge = new Discord.Client();
 
+
+doge.on('message', message => {     
+if (message.content.startsWith("DogeBOT --Standby")) {     
+if (message.author.id = (`${APIData.DogeAPIOwnerAuth1}`)) {
+message.channel.send("DogeBot is now on standby/update mode")
+doge.user.setStatus("idle")
+doge.user.setActivity("Please Stand By - Updating!")
+
+
+}
+  
+  if (message.author.id = (`${APIData.DogeAPIOwnerAuth2}`)) {
+message.channel.send("DogeBot is now on standby/update mode")
+doge.user.setStatus("idle")
+doge.user.setActivity("Please Stand By - Updating!")
+
+  }
+});
+
+  
+  doge.on('message', message => {   
+
 doge.login(process.env.DogeTK)
